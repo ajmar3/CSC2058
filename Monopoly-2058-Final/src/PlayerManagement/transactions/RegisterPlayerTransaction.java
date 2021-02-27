4136f7be5b3bbc302d3456ce2deb926907c3dbda
ajmar3 <75131408+ajmar3@users.noreply.github.com>
2021-02-27 16:02:19 +0000

    Revert "Add files via upload"
    This reverts commit bbd3e32cbf78ae89b4dd8d7b6133b81d0f1b9ddf.

--- Monopoly-2058-Final/src/PlayerManagement/transactions/RegisterPlayerTransaction.java
+++ Monopoly-2058-Final/src/PlayerManagement/transactions/RegisterPlayerTransaction.java
@@ -3,56 +3,28 @@ package PlayerManagement.transactions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import PlayerManagement.Player;
 
 public class RegisterPlayerTransaction {
 	
-	public int playerAmount = 0;
-	public static List<Player> gamePlayers = new ArrayList<Player>();
-	public Scanner nS = new Scanner(System.in);
-	
-	public List<Player> RegisterPlayers() {
-	
-		System.out.print("Enter the amount of players entering your game: ");
-        int amount = nS.nextInt();
-        nS.nextLine();
+	public int amountPlayers = 0;
+	public static List<Player> gamePlayers = new ArrayList<Player>();  //list of player objects
 
-        for(int i = 0; i < amount; i++){
-            System.out.println("Enter the name of player " + (i+1));
-            gamePlayers.add(new Player(nS.nextLine(), 1500));
-        }
-
-        if(!securityCheck()){
-            System.out.println("Some of the players entered are either null or not acceptable.");
-        }
-        else{
-            printDetails();
-            System.out.println("enter any button to start the game");
-            String in = nS.nextLine();
-            System.out.println("\n"+"#########################################################################"+"\n");   //adding row of #s after each turn  [A.M]
-            return gamePlayers;
-        }
-        return null;
+	public void RegisterPlayers() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the amount of players playing: ");    //getting the amount of players
+		Scanner input = new Scanner(System.in);
+		amountPlayers = input.nextInt();
+		input.close();
+		
+		for (int i=1; i<amountPlayers; i++) {
+			System.out.println("Enter the name of player " + i + ": ");
+			Scanner nameInput = new Scanner(System.in);
+			String playerName = nameInput.nextLine();
+			gamePlayers.add(new Player(playerName, 1500));
+			nameInput.close();
+		}
 	}
-	
-	public boolean securityCheck(){
-        for(int i = 0; i < gamePlayers.size(); i++){
-            if(gamePlayers.get(i) != null) {
-                return true;
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
-	
-    public static void printDetails(){
-        System.out.println("Here are the following players for the current game: ");
-        for(int x = 0; x < gamePlayers.size(); x++) {
-            if (gamePlayers.get(x) != null) {
-                gamePlayers.get(x).printDetails();
-            }
-        }
-    }
+
 }
