874e5bb665a2669916d739c61ff8468cea2dd684
teeradol123 <75262340+teeradol123@users.noreply.github.com>
2021-03-04 01:07:15 +0700

    Update TileInterface.java
    interface

--- Monopoly-2058-Final/src/TileManagement/TileInterface.java
+++ Monopoly-2058-Final/src/TileManagement/TileInterface.java
@@ -2,5 +2,25 @@ package TileManagement;
 
 public interface TileInterface {
 
-    
+    void printDetails();
+
+	public String getLocation();
+
+	void setLocation(String Location);
+
+	public String getName();
+
+	void setName(String name);
+
+	double getCost();
+
+	void setCost(double cost);
+
+	double getRent();
+
+	void setRent(double rent);
+
+	boolean isBuyable();
+
+	void setBuyable(boolean buyable);
 }
