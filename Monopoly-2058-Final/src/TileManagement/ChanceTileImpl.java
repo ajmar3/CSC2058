a9a70e660b3e491317a3625bf0e241344c099a0f
ansumandas2811 <74978243+ansumandas2811@users.noreply.github.com>
2021-03-05 16:50:34 +0000

    Update ChanceTileImpl.java

--- Monopoly-2058-Final/src/TileManagement/ChanceTileImpl.java
+++ Monopoly-2058-Final/src/TileManagement/ChanceTileImpl.java
@@ -1,5 +1,63 @@
 package TileManagement;
 
+import java.util.Random;
+import PlayerManagement.Player;
+
+
+
+
 public class ChanceTileImpl  implements ChanceTileInterface{
+	
+	
+	
+
+	public void drawCard(Player player, Tile tile) {
+		
+		
+Random rand = new Random();	
+int i = rand.nextInt(4);
+String[] chances = new String[4];
+ chances[0] = "Back to Go!";
+ chances[1] = "Go to Jail!";
+ chances[2] = "Back two spaces.";
+ chances[3] = "You won $100!";
+ chances[4] = "Pay $50 for NGOs on animal services.";
+
+
+
+if(i == 0) {
+	System.out.println(chances[0]);
+	Tile current = LoadTileTransaction.gameTiles.get(0);
+	player.setCurrentTile(current);
+	}
+else if(i == 1) {
+	System.out.println(chances[1]);
+	Tile current = LoadTileTransaction.gameTiles.get(30);
+	player.setCurrentTile(current);
+}
+else if(i == 2) {
+	System.out.println(chances[2]);
+	int temp = LoadTileTransaction.gameTiles.indexOf(player.getCurrentTile()) -2;
+	player.setCurrentTile(LoadTileTransaction.gameTiles.get(temp));
 
 }
+else if(i == 3) {
+	System.out.println(chances[3]);
+	double newBalance = player.getBalance() + 100.00;
+	player.setBalance(newBalance);
+	
+}
+else if(i == 4) {
+	System.out.println(chances[4]);
+	double newBalance = player.getBalance() - 50.00;
+	player.setBalance(newBalance);
+}
+	
+	
+	}
+	}
+
+
+
+
+
