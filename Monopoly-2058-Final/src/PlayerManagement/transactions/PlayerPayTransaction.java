9a90e902f8cc6409a6340d21b115ddc13efbb062
ansumandas2811 <74978243+ansumandas2811@users.noreply.github.com>
2021-03-05 16:49:44 +0000

    Update PlayerPayTransaction.java

--- Monopoly-2058-Final/src/PlayerManagement/transactions/PlayerPayTransaction.java
+++ Monopoly-2058-Final/src/PlayerManagement/transactions/PlayerPayTransaction.java
@@ -1,5 +1,40 @@
 package PlayerManagement.transactions;
 
 public class PlayerPayTransaction {
+package PlayerManagement.transactions;
+
+import PlayerManagement.Player;
+import TileManagement.Tile;
+
+public class PlayerPayTransaction {
+
+	public void playerBuy(Player player,Tile tile ){
+		if (!tile.buyable) return;
+		
+		if(tile.buyable) {
+			double newBalance = player.getBalance() - tile.getCost();
+			player.setBalance(newBalance);
+			player.addTile(tile);
+			
+			System.out.println("Player Balance = " + player.getBalance());
+			System.out.println("You now own = " + tile.getName());
+		}
+
+		
+		
+	}
+	
+	public void playerBuySafariPark(Player player, Tile tile) {
+		if (!tile.buyable) return;
+		if(tile.buyable) {
+			double newBalance = player.getBalance() - tile.getCostOfSafari();
+			player.setBalance(newBalance);
+			player.addSafariTiles(tile);
+			
+			System.out.println("Player Balance = " + player.getBalance());
+			System.out.println("You now own = " + tile.getName());
+		}
+	}
+}
 
 }
