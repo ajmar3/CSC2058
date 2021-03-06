65374532aab7d502b2272a0546087ed0da2a2470
teeradol123 <75262340+teeradol123@users.noreply.github.com>
2021-03-06 18:25:15 +0700

    Update PlayerPayTransaction.java

--- Monopoly-2058-Final/src/PlayerManagement/transactions/PlayerPayTransaction.java
+++ Monopoly-2058-Final/src/PlayerManagement/transactions/PlayerPayTransaction.java
@@ -7,16 +7,14 @@ import TileManagement.Tile;
 public class PlayerPayTransaction {
 
 	public void playerBuy(Player player,Tile tile ){
-		if (!tile.buyable) return;
-		
-		if(tile.buyable) {
+	
 			double newBalance = player.getBalance() - tile.getCost();
 			player.setBalance(newBalance);
 			player.addTile(tile);
 			
 			System.out.println("Player Balance = " + player.getBalance());
 			System.out.println("You now own = " + tile.getName());
-		}
+		
 
 		
 		
